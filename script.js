9b63b01d1228f75d0f984c4e8bc8123e18e191d0
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-06 14:50:52 -0500

    added 16 divs

---
+++ script.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 16; i++) {
+    const container = document.querySelector('#container');
+    const content = document.createElement('div');
+    container.appendChild(content);
+}
