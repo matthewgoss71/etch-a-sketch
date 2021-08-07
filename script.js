9c13d7b2c27d3682556f92a39f369a0ca7989f5b
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-07 10:00:00 -0500

    16x16 grid of divs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const container = document.querySelector('#container');
     const content = document.createElement('div');
+    content.classList.add('content');
     container.appendChild(content);
 }
