76613d2f27ffee252abbb1b66442bc265a513baf
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-07 11:06:08 -0500

    added changecolor on hover and removed internal borders

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ for (let i = 0; i < 256; i++) {
     const container = document.querySelector('#container');
     const content = document.createElement('div');
     content.classList.add('content');
+    content.setAttribute('id', 'div' + i);
     container.appendChild(content);
+}
+
+const gridSquares = document.querySelectorAll('.content');
+    Array.from(gridSquares).forEach((div) => {
+    let selectedSquare = div.addEventListener('mouseover', () => {
+    console.log(div.id);
+    addColor(div.id);
+  });
+});
+
+function addColor(selectedId) {
+    let changedSquare = document.getElementById(selectedId);
+    changedSquare.classList.add('add-color');
 }
