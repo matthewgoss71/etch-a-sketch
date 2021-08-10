c89ad753970f6d1483386fe65bec16b4df9e26f7
matthewgoss71 <matthewgoss71@gmail.com>
2021-08-09 21:46:34 -0500

    added rainbow mode and fixed reset function

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let x = 256;
 let y = 16;
-let z = 16;
-gridSizeSet(x, y, z);
+gridSizeSet(x, y);
 
-function gridSizeSet(x, y, z) {
+function gridSizeSet(x, y) {
     for (let i = 0; i < x; i++) {
-        newWidth = 480/y;
-        newHeight = 480/z;
+        newCellDim = 480/y;
         const container = document.querySelector('#container');
-        container.style.gridTemplateRows = 'repeat(' + z + ', ' + newHeight + 'px)';
-        container.style.gridTemplateColumns = 'repeat(' + y + ', ' + newWidth + 'px)';
+        container.style.gridTemplateRows = 'repeat(' + y + ', ' + newCellDim + 'px)';
+        container.style.gridTemplateColumns = 'repeat(' + y + ', ' + newCellDim + 'px)';
         const content = document.createElement('div');
         content.classList.add('content');
         content.setAttribute('id', 'div' + i);
-        content.style.height = newHeight + 'px';
-        content.style.width = newWidth + 'px';
+        content.style.height = newCellDim + 'px';
+        content.style.width = newCellDim + 'px';
         container.appendChild(content);
     }
         const gridSquares = document.querySelectorAll('.content');
@@ -29,24 +27,21 @@ const buttonReset = document.getElementById('reset');
 buttonReset.addEventListener('click', resetGrid); 
 
 function resetGrid() {
-    let changedSquares = document.querySelectorAll('.add-color');
+    let changedSquares = document.querySelectorAll('.content');
     let arrayChangedSquares = Array.from(changedSquares);
     for (let i = 0; i < arrayChangedSquares.length; i++) {
-        arrayChangedSquares[i].classList.remove('add-color');
+        arrayChangedSquares[i].style.removeProperty('background-color');
     }
 }
 
 const buttonResize = document.getElementById('resize');
 buttonResize.addEventListener('click', function resizeGridHeight() {
-    do {var newHeight = parseInt(prompt('Please enter new grid height:', ''))
-    } while (isNaN(newHeight) || newHeight < 1 || newHeight > 100);
-    do {var newWidth = parseInt(prompt('Please enter new grid width:', ''))
-    } while (isNaN(newWidth) || newWidth < 1 || newWidth > 100);
-    x = newHeight * newWidth;
-    y = newHeight;
-    z = newWidth;
+    do {var newDim = parseInt(prompt('Please enter new grid dimension:', ''))
+    } while (isNaN(newDim) || newDim < 1 || newDim > 100);
+    x = newDim * newDim;
+    y = newDim;
     setNewGrid();
-    gridSizeSet(x, y, z);
+    gridSizeSet(x, y);
 })
 
 function setNewGrid() {
@@ -62,7 +57,32 @@ const gridSquares = document.querySelectorAll('.content');
   });
 });
 
+const buttonBAW = document.querySelector('#black-and-white');
+buttonBAW.addEventListener('click', () => {
+        const gridSquares = document.querySelectorAll('.content');
+        Array.from(gridSquares).forEach((div) => {
+        div.addEventListener('mouseover', () => {
+        addColor(div.id);
+    });
+    });
+}); 
+
 function addColor(selectedId) {
     let changedSquare = document.getElementById(selectedId);
-    changedSquare.classList.add('add-color');
+    changedSquare.style.backgroundColor = 'black';
+}
+
+const buttonRainbow = document.querySelector('#rainbow');
+buttonRainbow.addEventListener('click', function rainbowColor() {
+    Array.from(document.querySelectorAll('.content')).forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            addRainbowColor(div.id);
+        });
+    });
+});
+
+function addRainbowColor(selectedId) {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16)
+    let changedSquare = document.getElementById(selectedId);
+    changedSquare.style.backgroundColor = '#' + randomColor;
 }
